4700ea9fb12005e558c6e10ef7f517dc29aa407d
Austie <wustinah20@gmail.com>
2022-12-21 20:46:29 -0800

    added apiKey and example request

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var apiKey = "7fd817a82bee4b8fbce597d0849507d8";
+var baseGeoUrl = "https://api.geoapify.com/v2/place-details?";
+
+var requestOptions = {
+    method: 'GET',
+};
+    
+fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=7fd817a82bee4b8fbce597d0849507d8", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
