13b3cf293b9a40ae9a486f4a0a950946f02cbb6c
Brianna Ligon <desmondsaelee@hotmail.com>
2022-12-28 20:55:50 -0800

    added on click button for search and creating for loop for 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var apiKey = "7fd817a82bee4b8fbce597d0849507d8";
+var apiKey = "179d01a2307062deab314b97c264567ad1a85bb0c6b8d15e038453be9cee7a60";
 var baseGeoUrl = "https://api.geoapify.com/v2/place-details?";
 
 var placeId, lat, lon;
@@ -11,16 +11,17 @@ var datesArray = $('.dates')
     
 function fetchLocationData(location){
     
-    var requestOptions = {
+    var requestOptions = { 
         method: 'GET',
     };
     
-    fetch(`https://api.geoapify.com/v1/geocode/search?text=${location}&apiKey=${apiKey}`, requestOptions)
+    fetch(`https://serpapi.com/search.json?engine=google_events&q=Events+in+Austin&hl=en&gl=us`, requestOptions)
     .then(response => response.json())
     .then(function(result){
         placeId = result.features[0].properties.place_id;
         lat = result.features[0].properties.lat;
         lon = result.features[0].properties.lon;
+        
     })
     .catch(error => console.log('error', error));
 
@@ -30,13 +31,15 @@ search.on('click', function () {
   $('#current').text(userInput + " " + today.format('dddd, MMMM D'))
   console.log(userInput)
   
+  
   });
 // const tomorrow = today.add(1,"day").format('dddd, MMMM D')
 // console.log(tomorrow)
 
 for (let index = 0; index < datesArray.length; index++) {
-    const element = datesArray[index];
-    const forecastDate = today.add(index+1,"day").format('dddd, MMMM D')
-    element.text(forecastDate)
+    const forecastIndex = datesArray[index];
+    const forecastDate = today.add(index++,"day").format('dddd, MMMM D')
+    $('.dates').text(forecastDate)
+    
 }
 
