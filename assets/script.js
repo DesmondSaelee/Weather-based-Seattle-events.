af4dd3e2a11d8bc0c926aa9e4b903729cf150dc6
Brianna Ligon <desmondsaelee@hotmail.com>
2023-01-01 16:49:33 -0800

    added fetch request for event and concerts. and added for loop. Now we need to figure out which information we want from the object and append it to the cards we want.added fetch request for event and concerts. and added for loop. Now we need to figure out which information we want from the object and append it to the cards we want.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var apiKey = "179d01a2307062deab314b97c264567ad1a85bb0c6b8d15e038453be9cee7a60";
 var baseGeoUrl = "https://api.geoapify.com/v2/place-details?";
-var serpApiKey = "179d01a2307062deab314b97c264567ad1a85bb0c6b8d15e038453be9cee7a60";
+var serpApiKey = "39ab25d4b3mshd3d6061f56936c2p1ccea5jsn16a8b8bc255b";
 var openWeatherApiKey = "9c26d768ead86b39036caf98fb0abbfa";
 
 var placeId, lat, lon;
@@ -10,24 +10,67 @@ var search = $('#searchBtn')
 var datesArray = $('.dates')
 
 // $('#current').append(today.format('dddd, MMMM D'));
-    
-function fetchLocationData(location){
-    
-    var requestOptions = { 
-        method: 'GET',
-    };
-    
-    fetch(`https://serpapi.com/search.json?engine=google_events&q=Events+in+Austin&hl=en&gl=us`, requestOptions)
-    .then(response => response.json())
-    .then(function(result){
-        placeId = result.features[0].properties.place_id;
-        lat = result.features[0].properties.lat;
-        lon = result.features[0].properties.lon;
-        
-    })
-    .catch(error => console.log('error', error));
 
-}
+// const SerpApi = require('google-search-results-nodejs');
+// const search = new SerpApi.GoogleSearch("xbfOzxgGLCmOGOhoRvKLZr6OzSHd1qus");
+
+// const params = {
+//   engine: "google_events",
+//   q: "Events in Austin",
+//   hl: "en",
+//   gl: "us"
+// };
+
+// const callback = function(data) {
+//   console.log(data["events_results"]);
+// };
+
+
+// search.json(params, callback);
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '39ab25d4b3mshd3d6061f56936c2p1ccea5jsn16a8b8bc255b',
+		'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
+	}
+};
+
+fetch('https://concerts-artists-events-tracker.p.rapidapi.com/venue?name=Hollywood%20bowl&page=1', options)
+	.then(response => response.json())
+	.then(function(response){
+        console.log(response)
+        for (var i = 0; i < response.data.length; i++) {
+            console.log(response.data[i].description)
+            // Put everything within the for loop between starting on line 43 and ending on 47. create elements and append to desired cards.
+
+        }
+       
+
+    })
+	.catch(err => console.error(err));
+
+  
+    
+// function fetchLocationData(location){
+    
+//     var requestOptions = { 
+//         mode: "no-cors",
+//         method: 'GET',
+//     };
+    
+//     fetch(`https://concerts-artists-events-tracker.p.rapidapi.com/venue`, requestOptions)
+//     .then(response => response.json())
+//     .then(function(result){
+//         placeId = result.features[0].properties.place_id;
+//         lat = result.features[0].properties.lat;
+//         lon = result.features[0].properties.lon;
+//         console.log("This is example" + result)
+//     })
+//     .catch(error => console.log('error', error));
+
+// }
+// fetchLocationData();
 
 function getPlaceDetails(){
 
