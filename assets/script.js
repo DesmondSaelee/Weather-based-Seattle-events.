145672b2e3ca54c9b7eff88c6daccb91b405264c
Brianna Ligon <desmondsaelee@hotmail.com>
2023-01-04 18:01:29 -0800

    We added functions for attaching weather info from api to cards

--- assets/script.js
+++ assets/script.js
@@ -15,37 +15,37 @@ var container = document.getElementById('artist-cards-container');
 function musicEvent() {
     const artistName = artistInput.value.trim();
     const options = {
-    method: 'GET',
-    headers: {
-    'X-RapidAPI-Key': '39ab25d4b3mshd3d6061f56936c2p1ccea5jsn16a8b8bc255b',
-    'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
-    }
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39ab25d4b3mshd3d6061f56936c2p1ccea5jsn16a8b8bc255b',
+            'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
+        }
     };
-    
+
     fetch(`https://concerts-artists-events-tracker.p.rapidapi.com/artist?name=${artistName}&page=1`, options)
-    
-    .then(response => response.json())
-    .then(function (response) {
-    console.log(response)
-    
-    function returnCards(response) {
-    return "<div class=\"artist-cards\">" + response.data.map(valuesCard => `
+
+        .then(response => response.json())
+        .then(function (response) {
+            console.log(response)
+
+            function returnCards(response) {
+                return "<div class=\"artist-cards\">" + response.data.map(valuesCard => `
     <div>
-    
+   
     <div class="artist-content">
     <h3 class=\"name-cards\"> Performing: ${valuesCard.name}</h3>
     <p>Venue: ${valuesCard.location.name}</p>
     <p>Peformance Date: ${valuesCard.startDate}</p>
-    </div> 
+    </div>
     </div>`).join('') + "</div>";
-    }
-    
-    container.innerHTML = returnCards(response);
-    })
-    .catch(err => console.error(err));
-    };
-    
-   
+            }
+
+            container.innerHTML = returnCards(response);
+        })
+        .catch(err => console.error(err));
+};
+
+
 
 
 
@@ -84,6 +84,32 @@ function fetchLocation(location) {
 }
 
 
+search.on('click', function () {
+    userInput = $('#first_name').val()
+    $('#current').text(userInput + " " + today.format('dddd, MMMM D'))
+    console.log(userInput)
+    fetchLocation(userInput)
+    musicEvent(userInput)
+    saveCity(userInput)
+});
+
+function saveCity(newcity) {
+    let cities = JSON.parse(localStorage.getItem('saved-cities')) || []
+    cities.push(newcity)
+    localStorage.setItem('saved-cities', JSON.stringify(cities))
+    // populateCities()
+}
+function populateCities(){
+    let cities = JSON.parse(localStorage.getItem('saved-cities')) || []
+    cities.map(function (city){
+        $button = $("button")
+        $button.text(city)
+        $('#cities-container').append($button)
+    })
+}
+
+
+
 function get5Day(lat, lon) {
     var forecastReq = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${openWeatherApiKey}`;
 
@@ -92,19 +118,38 @@ function get5Day(lat, lon) {
             return response.json();
         }
     }).then(function (data) {
-        //just logs the data for now
-        console.log(data);
+        let previousDate = ""
+        const nextFivedays = []
+        for (const item of data.list) {
+            if (item.dt_txt.split(" ")[0] != previousDate) {
+                nextFivedays.push(item)
+            }
+            previousDate = item.dt_txt.split(" ")[0]
+        }
+        populateFivedays(nextFivedays.slice(1))
+        populateCurrentcity(nextFivedays[0])
     });
 }
 
 
-search.on('click', function () {
-    userInput = $('#first_name').val()
-    $('#current').text(userInput + " " + today.format('dddd, MMMM D'))
-    console.log(userInput)
-    fetchLocation(userInput)
-    musicEvent(userInput)
+// my code
 
+$('.dates').each(function (index, element) {
+    const forecastDate = today.add(index++, "day").format('dddd, MMMM D')
+    $(this).text(forecastDate)
+})
 
+function populateFivedays(data) {
 
-});
+    $(".forecast-container .row").each(function (index, element) {
+        $(this).find("p").first().html(`Temp: ${data[index].main.temp} &#8457;`)
+        $(this).find("p").last().text(`Humidity: ${data[index].main.humidity} %`)
+        $(this).find("p:nth-child(3)").text(`Wind: ${data[index].wind.speed} MPH`)
+    })
+}
+
+function populateCurrentcity(data) {
+    $(".main-card p").first().html(`Temp: ${data.main.temp} &#8457;`)
+    $(".main-card p").last().text(`Humidity: ${data.main.humidity} %`)
+    $(".main-card p:nth-child(3)").text(`Wind: ${data.wind.speed} MPH`)
+}
